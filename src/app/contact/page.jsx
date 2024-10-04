22b48714279b8f5a01eb449e90c2d8d5d6c3e3f5
OlimovN <olimonurmuhammad711@gmail.com>
2024-10-04 13:57:25 +0500

    add clas

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ContactPages() {
+  return (
+    <div>ContactPages</div>
+  )
+}
---
+++ src/app/product/[id]/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function IdDetails() {
+  return (
+    <div>IdDetails</div>
+  )
+}
